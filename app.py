6a7e5edd7edcb95b6237491fbfac6f7314b9eac4
Akash Sharma <xufor.developer.official@gmail.com>
2019-06-09 15:30:36 +0530

    Reverting changes from last commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from marshmallow import ValidationError
 
+from db import db
+from ma import ma
 from resources.signup import SignUp
 from resources.confirm import Confirm
 
@@ -13,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 api = Api(app)
+db.init_app(app)
 
 api.add_resource(SignUp, '/signup')
 api.add_resource(Confirm, '/confirm')
@@ -24,8 +27,5 @@ def handle_marshmallow_validation(err):
 
 
 if __name__ == '__main__':
-    from db import db
-    from ma import ma
-    db.init_app(app)
     ma.init_app(app)
     app.run(port=5000, debug=True)
