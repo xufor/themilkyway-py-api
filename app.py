f178f68aa257fe90f5695132a9dd730a2cda5136
Akash Sharma <xufor.developer.official@gmail.com>
2019-06-09 14:47:01 +0530

    Minor Enhancement

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from marshmallow import ValidationError
 
-from db import db
-from ma import ma
 from resources.signup import SignUp
 from resources.confirm import Confirm
 
@@ -15,7 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 api = Api(app)
-db.init_app(app)
 
 api.add_resource(SignUp, '/signup')
 api.add_resource(Confirm, '/confirm')
@@ -27,5 +24,8 @@ def handle_marshmallow_validation(err):
 
 
 if __name__ == '__main__':
+    from db import db
+    from ma import ma
+    db.init_app(app)
     ma.init_app(app)
     app.run(port=5000, debug=True)
