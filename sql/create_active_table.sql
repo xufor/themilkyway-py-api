7f8932a83339584c9f6aeb873fcc367b49e067b0
Akash Sharma <xufor.developer.official@gmail.com>
2019-06-16 00:08:13 +0530

    Removed the sql folder.

--- sql/create_active_table.sql
+++
@@ -1,6 +0,0 @@
-create table active(
-    uid varchar(6) primary key,
-    name varchar(80) not null,
-    email varchar(100) not null unique,
-    password varchar(60) not null
-                   );
--- sql/create_inactive_table.sql
+++
@@ -1,6 +0,0 @@
-create table inactive(
-    email varchar(100) primary key,
-    name varchar(80) not null,
-    password varchar(60) not null,
-    code varchar(32) not null unique
-                     );
