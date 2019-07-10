199cbe375ae4156a3ffc4daa47985aa84492a285
Akash Sharma <xufor.developer.official@gmail.com>
2019-07-10 20:17:35 +0530

    A bug fix.

--- resources/profile.py
+++ resources/profile.py
@@ -48,13 +48,12 @@ class Profile(Resource):
         # Creating an for the requesting active user
         active_user_object = ActiveModel.find_entry_by_uid(current_user)
         # Check if basic data already exists
-        # If no image was uploaded the the frontend is expected to send the field empty.
         existing_basic_object = active_user_object.basic
-        if not existing_basic_object:
+        if existing_basic_object is None:
             fresh_basic_object.uid = current_user
             if fresh_basic_object.create_entry() == ERROR_WRITING_BASIC_TABLE:
-                return {'message': UPDATE_UNSUCCESSFUL}
-            return {'message': UPDATE_SUCCESSFUL}
+                return {'message': UPDATE_UNSUCCESSFUL}, 500
+            return {'message': UPDATE_SUCCESSFUL}, 201
         else: 
             # Update all fields with new data
             existing_basic_object.bio = fresh_basic_object.bio
