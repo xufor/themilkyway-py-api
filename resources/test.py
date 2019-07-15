f8c25254af0405b027e75e8ac6661e51db454b65
Akash Sharma <xufor.developer.official@gmail.com>
2019-07-15 12:07:52 +0530

    Audited test endpoint,

--- resources/test.py
+++ resources/test.py
@@ -1,9 +1,7 @@
 from flask_restful import Resource
-from models.active import StoryModel
 
 
 class Test(Resource):
     @classmethod
     def get(cls):
-        x = StoryModel.find_feed_stories_by_genre('Classic', 1)
-        return {'result': [StoryModel.generate_story_element_data(obj) for obj in x]}
+        return {"message": "working!"}
