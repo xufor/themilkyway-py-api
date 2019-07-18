034038c7e61a7dade69107362508cd2881e3f7f1
Akash Sharma <xufor.developer.official@gmail.com>
2019-07-18 23:37:05 +0530

    Everything normal now!

--- resources/test.py
+++ resources/test.py
@@ -1,10 +1,7 @@
-import os
 from flask_restful import Resource
-
-ADMIN_UID = os.getenv('ADMIN_USER', '26e1a2')
 
 
 class Test(Resource):
     @classmethod
     def get(cls):
-        return {"message": ADMIN_UID == "ffffff"}
+        return {"message": "working!"}
